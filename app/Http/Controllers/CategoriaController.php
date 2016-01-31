d90452fca841ab458443e2ae3b14f35e027a924c
fabiomoura.ads <fabiomoura.ads@gmail.com>
2016-01-31 17:59:05 -0200

    Atualização da classe CategoriaController

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,67 +12,134 @@ use App\Categoria;
 class CategoriaController extends Controller
 {
 	
+	protected $categoria;
+	protected $CLASS_NAME = "Categoria";
+	
+	public function __construct(Categoria $categoria){
+		$this->categoria = $categoria;
+		$erros = array();
+	}
+	
 	public function getIndex(){
 		return Categoria::all();
 	}
 	
-	/*
-		Criado a getInserir para testar mais facil, ele só chama o post, quando for pra produção comentar esse método;
-		Por: Fábio Moura, 31/01/2016
-	*/
-	public function getInserir(Request $request){
-		return $this->postInserir($request);
-	}
-	
-	/**
-	 * Método postInserir(), recebe o request e pode trabalhar com todos os parêmtros 
-	 * do formulário. O nome é inserido com a primeira letra maiúscula, para padronizar;
-	 * Por: Fábio Moura, 31/01/2016
-	 **/
-	public function postInserir(Request $request){		
-		$nome = $request->input("nome");
+	public function postStore($nome){		
+		$funcao = "postStore()";
+
+		$this->validaPassagemDeParametro($funcao, "nome", $nome, null);
+		
+		if ( count($this->erros) > 0 ) {
+			return $this->erros;
+		}		
+				
 		$nome = strtolower($nome);
 		$nome = ucfirst($nome);	
 		
-		$categoria = new Categoria;
+		$categoria = $this->categoria;
 		$categoria->nome = $nome;
-		$categoria->save();
 		
-		return "Categoria ".$categoria->nome. " inserida com sucesso!";				
+		if ( $categoria->save() ) {			
+			$mensagem = $this->getMessageReturn("success", "inserida com sucesso!", $nome );			
+		} else {
+			$mensagem = $this->getMessageReturn("error", "não foi inserida, verifique!", $nome );			
+		}
+		
+		return $mensagem;
 	}	
 	
-	public function getBuscar(Request $request){
+	public function getShow($id){
+		$funcao = "getShow()"; 
+			
+		$this->validaPassagemDeParametro($funcao, "id", $id, null);
 		
-		$id = $request->input("id");
-		$categoria = new Categoria;
+		if ( count($this->erros) > 0 ) {
+			return $this->erros;
+		}
+		
+		$categoria = $this->categoria;
 		$categoria = $categoria->find($id);
+		
+		if ( !$categoria ) {
+			return $this->getMessageReturn("error", "não foi localizada!", $id);			
+		}
 
 		return $categoria;
 	}
 	
-	public function postAtualizar(Request $request){
-		$id = $request->input("id");
-		$nomeAtual = $request->input("nome");
-				
-		$categoria = new Categoria;
-		$categoria = $categoria->find($id);		
-		$nomeAnterior = $categoria->nome;	
-
-		$categoria->update(["nome" => $nomeAtual]);
+	public function postUpdate(Request $request){
+		$funcao = "postUpdate()";
 		
-		return "Categoria ". $nomeAnterior. " atualizada para ". $nomeAtual;			
+		$id = $request->input("id");
+		$nome = $request->input("nome");
+		
+		$this->validaPassagemDeParametro($funcao, "id", $id, null);
+		$this->validaPassagemDeParametro($funcao, "nome", $nome, null);
+		
+		if ( count($this->erros) > 0 ) {
+			return $this->erros;
+		}			
+				
+		$categoria = $this->categoria->find($id);
+		
+		if ( !$categoria ) {
+			return $this->getMessageReturn("error", "nao foi localizada", $id);			
+		} 
+		
+		if ( $categoria->update(["nome" => $nome]) ) {
+			return $this->getMessageReturn("success", "atualizada com sucesso!", $nome);			
+		}
+		
+		return $this->getMessageReturn("error", "nao foi atualizada, verifique!", $id);			
+	}
+		
+	public function postDestroy($id){
+		$funcao = "postDestroy()";
+		
+		$this->validaPassagemDeParametro($funcao, "id", $id, null);
+		
+		if ( count($this->erros) > 0 ) {
+			return $this->erros;
+		}	
+
+		$categoria = $this->categoria->find($id);
+
+		if ( $categoria ) {
+			$nome = $categoria->nome;
+			
+			if ( $categoria->delete() ){ 
+				return $this->getMessageReturn("success", "excluida com sucesso!", $nome);			
+			} 
+			
+			return $this->getMessageReturn("error", "nao foi excluida, verifique!", $id);			
+		}
+		
+		return $this->getMessageReturn("error", "nao foi localizada!", $id);			
+		
 	}
 	
-	public function postDeletar(Request $request){
-		$id = $request->input("id");
-		$categoria = new Categoria;
-		$categoria = $categoria->find($id);
-		if ( $categoria != null ) {
-			$nome = $categoria->nome;
-			$categoria->delete();
-			return "Categoria ".$nome." excluída com sucesso!";
-		}  
+	/**
+	 * Método getMessageReturn() foi criado para encapsular as mensagens de retorno para view,
+	 * sendo parametrizável para qualque tipo de retorno, atráves dos parametros recebidos;
+	 * $tipo = Tipo de mensagem de retorno, "sucess" ou "error"; ( obrigatório )
+	 * $mensagem = Mensagem que será retornada; ( obrigatório )
+	 * $nome = Nome do registro que está sendo manipulado ( opcional ) 
+	 * Por: Fábio Moura, em 31/01/2016
+	 **/
+	public function getMessageReturn($tipo, $mensagem, $nome){
 		
-		return "Não foi localizado categoria de id ".$id;	
+		if ( $nome ) {
+			$mensagem = $this->CLASS_NAME." ".$nome." ".$mensagem;	
+		} else {
+			$mensagem = $this->CLASS_NAME." ".$mensagem;				
+		} 
+		
+		$resposta = [$tipo => $mensagem];
+		return $resposta;
 	}
+		
+	public function getFail($acao){
+		return ["error" => "Não foi possível ".$acao." o registro!\nEntre em contato com o administrador do sistema."];
+	}	
+
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,5 +10,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public $erros = array();
+	
+	/**
+	 * Método para validar a passagem de parâmetro para uma função.
+	 * Parametros: 
+	 * $funcao = Nome da função que está sendo executada;
+	 * $parametro = Nome do parâmetro a ser verificado;
+	 * $valor = Valor do parêmtro para ser verificado se existe;
+	 * $tipo = Opcional, para validar o tipo de valor
+	 * Por: Fábio Moura, em 31/01/2016
+	 **/
+	public function validaPassagemDeParametro($funcao, $parametro, $valor, $tipo){		
+		if ( $valor == null) {
+			$mensagem = ["error", "Parametro '".$parametro."' obrigatorio para chamada da funcao ".$funcao.", verifique!"];
+			array_push($this->erros, $mensagem);		
+		}
+	}	
 	
 }
