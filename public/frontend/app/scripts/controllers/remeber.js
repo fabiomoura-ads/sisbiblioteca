a88ebc160c357c8ed9a318c4b02df9f732bd2971
Haelio Márcio <haelioferreira@yahoo.com.br>
2016-02-27 00:31:24 -0300

    Ajustes

---
+++ public/frontend/app/scripts/controllers/remeber.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:RemeberCtrl
+ * @description
+ * # RemeberCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('RemeberCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
